32a4211dcddfc59b8d464a126b206873abfeaf81
Leckie <laixianbo@bbdservice.com>
2018-11-09 09:24:12 +0800

    liquidatabase tests

---
+++ juggling-liquibase/src/main/java/me/leckie/liquibase/LiquibaseApplication.java
@@ -0,0 +1,20 @@
+/**
+ * BBD Service Inc All Rights Reserved @2018
+ */
+package me.leckie.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author laixianbo
+ * @version $Id: LiquibaseApplication.java, v0.1 2018/11/8 18:27 laixianbo Exp $$
+ */
+@SpringBootApplication
+public class LiquibaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiquibaseApplication.class, args);
+  }
+
+}
