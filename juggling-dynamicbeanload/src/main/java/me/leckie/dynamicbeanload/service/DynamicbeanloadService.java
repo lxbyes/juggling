40fed5b7fec37ee4b0456377206dc9c51fdb9b8e
Leckie <603418904@qq.com>
2018-11-13 02:14:47 +0800

    just tests

--- juggling-dynamicbeanload/src/main/java/me/leckie/dynamicbeanload/service/DynamicbeanloadService.java
+++ juggling-dynamicbeanload/src/main/java/me/leckie/dynamicbeanload/service/DynamicbeanloadService.java
@@ -1,6 +1,10 @@
 package me.leckie.dynamicbeanload.service;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionReader;
@@ -30,10 +34,23 @@ public class DynamicbeanloadService implements ApplicationContextAware {
     // 之前的对象依然被引用
     System.out.println(dynamicService);
     System.out.println(applicationContext.containsBean(beanName));
+    URLClassLoader urlClassLoader = new URLClassLoader(new URL[100]);
+    try {
+      urlClassLoader.getURLs()[0] = new URL("http://asdsdfa.com");
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    }
+    beanFactory.setTempClassLoader(urlClassLoader);
+    beanFactory.setBeanClassLoader(urlClassLoader);
+    beanFactory.setCacheBeanMetadata(false);
+    beanFactory.clearMetadataCache();
+    beanFactory.setAllowBeanDefinitionOverriding(true);
+    System.out.println(beanFactory.getBeanClassLoader().getClass().getName());
+    System.out.println(beanFactory.getBeanClassLoader().getParent().getClass().getName());
     // 获取bean时重新被初始化
     DynamicService dynamicServiceNew = applicationContext.getBean(DynamicService.class);
     System.out.println(dynamicServiceNew);
     System.out.println(dynamicService == dynamicServiceNew); //false
-
+    System.out.println(dynamicServiceNew.getClass().getClassLoader().getClass().getName());
   }
 }
