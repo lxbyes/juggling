a04e9ec7211898ce552e7a07ac7b2d0762692487
Leckie <laixianbo@bbdservice.com>
2019-05-16 20:45:02 +0800

    DataJpaTest Modifying

--- juggling-jpa/src/main/java/me/leckie/juggling/jpa/repository/StudentRepository.java
+++ juggling-jpa/src/main/java/me/leckie/juggling/jpa/repository/StudentRepository.java
@@ -4,11 +4,13 @@ import me.leckie.juggling.jpa.domain.dataobject.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Leckie
  * @version $Id: StudentRepository.java, v0.1 2019/5/15 15:09 john Exp $$
  */
+@Transactional
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     @Modifying(clearAutomatically = true)
@@ -26,5 +28,13 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @Modifying(flushAutomatically = true)
     @Query(value = "update Student t set t.gender=?2 where t.id=?1")
     void updateGenderFlushAutomatically(Long id, String gender);
+
+    @Modifying
+    @Query(value = "update Student t set t.name=?2 where t.id=?1")
+    void updateName(Long id, String name);
+
+    @Modifying
+    @Query(value = "update Student t set t.gender=?2 where t.id=?1")
+    void updateGender(Long id, String gender);
 
 }
--- juggling-jpa/src/test/java/me/leckie/juggling/jpa/repository/StudentRepositoryTests.java
+++ juggling-jpa/src/test/java/me/leckie/juggling/jpa/repository/StudentRepositoryTests.java
@@ -1,11 +1,15 @@
 package me.leckie.juggling.jpa.repository;
 
+import me.leckie.juggling.jpa.domain.dataobject.Student;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Leckie
@@ -20,7 +24,36 @@ public class StudentRepositoryTests {
     private StudentRepository studentRepository;
 
     @Test
+    @Transactional
+    @Commit
     public void test() {
+        studentRepository.flush();
+        Student student2 = studentRepository.findById(2L).orElse(null);
+        Student student3 = studentRepository.findById(3L).orElse(null);
+        studentRepository.updateName(2l, "Vickie123");
+        student2.setName("Vickie-set123");
+        student3.setName("Leckie-set123");
+    }
+
+    @Test
+    @Commit
+    public void testFlush() {
+        Student student = studentRepository.findById(2L).orElse(null);
+        Assert.assertNotNull(student);
+        student.setGender("FEMALE");
+        studentRepository.updateNameFlushAutomatically(2l, "Leckie");
+        Assert.assertEquals("FEMALE", student.getGender());
+    }
+
+    @Test
+    @Commit
+    public void testClear() {
+        Student student = studentRepository.findById(2L).orElse(null);
+        Assert.assertNotNull(student);
+        student.setGender("FEMALE");
+        studentRepository.updateNameClearAutomatically(3l, "Leckie5");
+        Assert.assertEquals("FEMALE", student.getGender());
+        Assert.assertEquals("MALE", studentRepository.findById(2L).get().getGender());
     }
 
 }
